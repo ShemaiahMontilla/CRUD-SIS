466f4aa7663c759cd16c320245fd9377228d1d2f
ShemaiahMontilla <shemaiah.montilla@g.msuiit.edu.ph>
2022-05-12 09:41:16 +0800

    Adding the code for the connection of database

---
+++ DB_Connection.py
@@ -0,0 +1,24 @@
+
+   
+import mysql.connector
+
+class DB_Connect:
+	def __init__(self):
+		pass
+
+	def connection(self):
+		self.con = mysql.connector.connect(
+		  host="localhost",
+		  user="root",
+		  password="AuntJoy@0329",
+		  database="students3"
+		)
+		return self.con
+
+
+db = DB_Connect()
+
+con = db.connection()
+mycursor = con.cursor()
+
+print(con)
